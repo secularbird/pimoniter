8176b9c5c4b9ef48f4e95f67719adb5f2e173b10
zhuangyao <secularbird.eagle@gmail.com>
2017-07-30 22:03:40 +0800

    add weather monitor

--- agents/sysmonitor.py
+++ agents/sysmonitor.py
@@ -2,7 +2,8 @@ import psutil
 from pymongo import MongoClient
 import datetime
 import threading
-
+import requests
+import json
 
 client = MongoClient()
 db = client.sysinfo
@@ -15,11 +16,28 @@ def get_sensor_temp():
         return int(temp1.read())
 
 
+def get_local_weather_temp():
+    '''get local weather temp which using amap api'''
+    weatherapi = "http://restapi.amap.com/v3/weather/weatherInfo?key=bb46dc7c8cfb9f2b89b46f26c0b41e7f&city=310104&extensions=base"
+    req = requests.get(weatherapi)
+    j = json.loads(req.text)
+    if j["info"] == "OK":
+        return j["lives"][0]["temperature"]
+    else:
+        return 0
+
+
+def get_cpu_percent():
+    return psutil.cpu_percent(interval=None)
+
+
 def record_db():
 
     temp = get_sensor_temp()
-    recorde_time = datetime.datetime.now()
-    temp_data = {"temp": temp, "time": recorde_time}
+    weather_temp = get_local_weather_temp()
+    record_time = datetime.datetime.now()
+    cpu_percent = get_cpu_percent()
+    temp_data = {"temp": temp, "weather_temp": weather_temp, "cpu_percent": cpu_percent, "time": record_time}
     print(temp_data)
     db.temp.insert(temp_data)
 
